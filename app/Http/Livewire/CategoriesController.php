b66621f55e4bc6434da2564df8632192622e4187
alexDev2808 <colores.2808.8000@gmail.com>
2024-03-19 12:36:55 -0600

    Primer componente con livewire: Categories

---
+++ app/Http/Livewire/CategoriesController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Category;
+use Livewire\Component;
+use Livewire\WithFileUploads; // trait -> img a back
+use Livewire\WithPagination;
+use Illuminate\Support\Facades\Storage;
+
+class CategoriesController extends Component
+{
+    use WithFileUploads;
+    use WithPagination;
+
+    public $name, $search, $image, $selected_id, $pageTitle, $componentName;
+    private $pagination = 5;
+
+    public function render()
+    {
+        $data = Category::all();
+
+        return view('livewire.category.categories', [ 'categories' => $data ])
+                ->extends('layouts.theme.app')
+                ->section('content');
+    }
+}
