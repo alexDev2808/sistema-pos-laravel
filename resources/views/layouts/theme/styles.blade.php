4390fe0f9e77afa0a1f06ff45cbd8737525501ca
alexDev2808 <colores.2808.8000@gmail.com>
2024-03-18 23:36:38 -0600

    Personalizando plugins

--- resources/views/layouts/theme/styles.blade.php
+++ resources/views/layouts/theme/styles.blade.php
@@ -5,8 +5,46 @@
 <link href="{{ asset('assets/css/plugins.css') }}" rel="stylesheet" type="text/css" />
 <link href="{{ asset('assets/css/structure.css') }}" rel="stylesheet" type="text/css" class="structure" />
 
-<!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM STYLES -->
-<link href="{{ asset('plugins/apex/apexcharts.css') }}" rel="stylesheet" type="text/css">
-<link href="{{ asset('assets/css/dashboard/dash_2.css') }}" rel="stylesheet" type="text/css" class="dashboard-sales" />
-<!-- END PAGE LEVEL PLUGINS/CUSTOM STYLES -->
+<link rel="stylesheet" href="{{ asset('plugins/font-icons/fontawesome/css/fontawesome.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/css/elements/avatar.css') }}">
+<link rel="stylesheet" href="{{ asset('plugins/sweetalerts/sweetalert.css') }}">
+<link rel="stylesheet" href="{{ asset('plugins/notification/snackbar/snackbar.min.css') }}">
+
+{{--  --}}
+<link rel="stylesheet" href="{{ asset('css/fontawesome.css') }}">
+<link rel="stylesheet" href="{{ asset('css/custom.css') }}"> 
+
+<link rel="stylesheet" href="{{ asset('assets/css/widgets/modules-widgets.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/css/forms/theme-checkbox-radio.css') }}">
+
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+<style>
+    aside {
+        display: none!important;
+    }
+    .page-item.active .page-link {
+        z-index: 3;
+        color: #FFF;
+        background-color: #3B3F5C;
+        border-color: #3B3F5C;
+    }
+
+    @media ( max-width: 480px ) {
+        .mtmobile {
+            margin-bottom: 20px!important;
+        }
+        .mbmobile {
+            margin-bottom: 10px!important;
+        }
+        .hideonsm {
+            display: none!important;
+        }
+        .inblock {
+            display: block;
+        }
+    }
+
+</style>
+
+@livewireStyles
