0b4784ffab953ce1ea34a7c4a4d6daa7aa0ebdbc
alexDev2808 <colores.2808.8000@gmail.com>
2024-03-20 09:57:20 -0600

    Vista del componente Categories

--- resources/views/livewire/category/categories.blade.php
+++ resources/views/livewire/category/categories.blade.php
@@ -4,15 +4,16 @@
         <div class="widget widget-chart-one">
             <div class="widget-heading">
                 <h4 class="card-title">
-                    <b>Component name | PageTitle</b>
+                    <b>{{ $componentName }} | {{ $pageTitle }}</b>
                 </h4>
                 <ul class="tabs tab-pills">
                     <li>
                         <a 
                             href="javascript:void(0)" 
-                            class="tabmenu bg-dark " 
+                            class="tabmenu" 
                             data-toggle="modal " 
-                            data-target="#theModal" >
+                            data-target="#theModal"
+                            style="background: #5356FF">
                             Agregar
                         </a>
                     </li>
@@ -25,7 +26,7 @@
 
                 <div class="table-responsive ">
                     <table class="table table-bordered table-striped mt-1">
-                        <thead class="text-white bg-dark ">
+                        <thead class="text-white" style="background: #378CE7">
                             <tr>
                                 <th class="table-th text-white ">Descripcion</th>
                                 <th class="table-th text-white ">Imagen</th>
@@ -33,35 +34,42 @@
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td><h6>Category name</h6></td>
-                                <td class="text-center">
-                                    <span>
-                                        <img 
-                                            src="" 
-                                            alt="Img de ejemplo" 
-                                            height="70" 
-                                            width="80" 
-                                            class="rounded">
-                                    </span>
-                                </td>
-                                <td class="text-center">
-                                    <a 
-                                        href="javascript:void(0)"
-                                        class="btn btn-dark mt-mobile"
-                                        title="Edit"
-                                        >
-                                        <i class="fas fa-edit"></i>
-                                    </a>
-                                    <a 
-                                        href="javascript:void(0)"
-                                        class="btn btn-dark"
-                                        title="Delete"
-                                        >
-                                        <i class="fas fa-trash"></i>
-                                    </a>
-                                </td>
-                            </tr>
+                            @foreach ($categories as $category)
+                                <tr>
+                                    <td>
+                                        <h6>{{ $category->name }}</h6>
+                                    </td>
+                                    <td class="text-center">
+                                        <span>
+                                            <img 
+                                                src="{{ asset('storage/categories/' . $category->image ) }}" 
+                                                alt="Img de ejemplo" 
+                                                height="70" 
+                                                width="80"
+                                                class="rounded">
+                                        </span>
+                                    </td>
+                                    <td class="text-center">
+                                        <a 
+                                            href="javascript:void(0)" 
+                                            wire:click="Edit({{ $category->id }})"
+                                            class="btn mt-mobile" 
+                                            title="Edit"
+                                            style="background: #FFC700">
+                                            <i class="fas fa-edit"></i>
+                                        </a>
+                                        <a 
+                                            href="javascript:void(0)" 
+                                            onclick="Confirm('{{ $category->id }}')"
+                                            class="btn text-white" 
+                                            title="Delete"
+                                            style="background: #FF204E">
+                                            <i class="fas fa-trash"></i>
+                                        </a>
+                                    </td>
+                                </tr>
+                            @endforeach
+
                         </tbody>
                     </table>
                     Pagination
@@ -78,6 +86,51 @@
 
 <script>
     document.addEventListener('DOMContentLoaded', () => {
-        
-    })
+
+        window.livewire.on('category-added', msg => {
+            $('#theModal').modal('hide');
+            noty(msg)
+        })
+
+        window.livewire.on('category-updated', msg => {
+            $('#theModal').modal('hide');
+            noty(msg)
+        })
+
+        window.livewire.on('category-deleted', msg => {
+            noty(msg)
+        })
+
+        window.livewire.on('hide-modal', msg => {
+            $('#theModal').modal('hide');
+        })
+
+        window.livewire.on('show-modal', msg => {
+            $('#theModal').modal('show');
+        })
+
+        window.livewire.on('hidden.bs.modal', msg => {
+            $('.er').css('display', 'none');
+        })
+
+    });
+
+    function Confirm( id ) {
+        swal({
+            title: 'Confirmar',
+            text: 'Confirmas eliminar el registro?',
+            type: 'warning',
+            showCancelButton: true,
+            cancelButtonText: 'Cerrar',
+            cancelButtonColor: '#FFF',
+            confirmButtonText: 'Aceptar',
+            confirmButtonColor: '#5356FF'
+        }).then(function( res ) {
+            if( res.value ){
+                window.livewire.emit( 'deleteRow', id )
+                swal.close()
+            }
+        })
+    }
+
 </script>
