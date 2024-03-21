7ba8b8221e46139637d003be333e3baaa14a994c
alexDev2808 <colores.2808.8000@gmail.com>
2024-03-21 10:18:09 -0600

    Products: Metodo Update

--- app/Http/Livewire/ProductsController.php
+++ app/Http/Livewire/ProductsController.php
@@ -103,4 +103,78 @@ class ProductsController extends Component
         $this->emit('product-added', 'Producto agregado');
 
     }
+
+    public function Edit( Product $product ) {
+
+        $this->selected_id = $product->id;
+        $this->name = $product->name;
+        $this->barcode = $product->barcode;
+        $this->price = $product->price;
+        $this->cost = $product->cost;
+        $this->stock = $product->stock;
+        $this->alerts = $product->alerts;
+        $this->category_id = $product->category_id;
+        $this->image = null;
+
+        $this->emit('show-modal', 'Show modal');
+
+    }
+
+    public function Update() {
+        $rules = [
+            'name' => "required|min:3|unique:products,name,{$this->selected_id}",
+            'cost' => 'required',
+            'price' => 'required',
+            'stock' => 'required',
+            'alerts' => 'required',
+            'category_id' => 'required|not_in:Elegir'
+        ];
+
+        $messages = [
+            'name.required'=> 'El nombre es requerido',
+            'name.unique' => 'Ya existe ese nombre',
+            'name.min' => 'El nombre debe tener minimo 3 caracteres',
+            'cost.required' => 'El costo es requerido',
+            'price.required' => 'El precio es requerido',
+            'stock.required' => 'El stock es requerido',
+            'alerts.required' => 'El valor minimo de existencias es requerido',
+            'category_id.not_in' => 'Elige un nombre diferente a Elegir',
+        ];
+
+        $this->validate($rules, $messages);
+
+        $product = Product::find($this->selected_id);
+
+        $product->update([
+            'name' => $this->name,
+            'cost' => $this->cost,
+            'price'=> $this->price,
+            'barcode' => $this->barcode,
+            'stock' => $this->stock,
+            'alerts' => $this->alerts,
+            'category_id' => $this->category_id,
+        ]);
+
+        if( $this->image ) {
+            $customFileName = uniqid() . '_.' . $this->image->extension();
+            $this->image->storeAs('public/productos', $customFileName);
+            $imageTemp = $product->image;
+            $product->image = $customFileName;
+
+            $product->save();
+
+            if( $imageTemp != null ) {
+                if( file_exists( 'storage/productos/' . $imageTemp ) ) {
+                    unlink( 'storage/productos/'. $imageTemp );
+                }
+            }
+        }
+
+        $this->resetUI();
+        $this->emit('product-updated', 'Producto actualizado');
+
+    }
+
+
+
 }
