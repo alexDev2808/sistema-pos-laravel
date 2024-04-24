9bd144ed46622e90934116c0a889bc6befb393b7
alexhDev2808 <colores.2808.8000@gmail.com>
2024-04-24 08:58:19 -0600

    Ventas: Vistas parciales

--- resources/views/livewire/pos/posComponent.blade.php
+++ resources/views/livewire/pos/posComponent.blade.php
@@ -1,6 +1,29 @@
 
 
 <div>
-    {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
-    <h1>Ventas</h1>
+    <style></style>
+
+    <div class="row layout-top-spacing">
+
+        <div class="col-sm-12 col-md-8">
+            {{-- Detalles --}}
+            @include('livewire.pos.partials.detail')
+
+        </div>
+
+        <div class="col-sm-12 col-md-4">
+            {{-- Total --}}
+            @include('livewire.pos.partials.total')
+
+            {{-- Coins --}}
+            @include('livewire.pos.partials.coins')
+        </div>
+    </div>
 </div>
+
+
+<script>
+
+
+
+</script>
